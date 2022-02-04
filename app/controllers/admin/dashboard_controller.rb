75016684d9746e5abc4679683164edd00af30a23
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-02-04 22:21:47 +0000

    add addtional info on admin dashboard page (count of both products and categories in db)

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   
   def show
+    @product_count = Product.all.size
+    @category_count = Category.all.size
   end
 end
